226f48ad12dac252203d42ac684cceacaf29124f
Erik <59744487+knottem@users.noreply.github.com>
2024-02-25 13:59:24 +0100

    Fixed broken weather tables

--- sql/0002-weathercache.sql
+++ sql/0002-weathercache.sql
@@ -2,27 +2,31 @@
 USE `weatherdb`;
 -- Create the Cache table
 
-CREATE TABLE `weather` (
-`id` VARCHAR(36) NOT NULL,
-`time_stamp` datetime(6) DEFAULT NULL,
-`message` varchar(255) DEFAULT NULL,
-`city` varchar(255) DEFAULT NULL,
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table weather
+(
+    id         varchar(36)                            not null
+        primary key,
+    time_stamp datetime(6)                            null,
+    message    varchar(255)                           null,
+    city_id    varchar(36) collate utf8mb4_unicode_ci null,
+    constraint FK_weather_city
+        foreign key (city_id) references city (id)
+);
 
-CREATE TABLE `weather_data` (
-`id` VARCHAR(36) NOT NULL,
-`precipitation` float DEFAULT NULL,
-`temperature` float DEFAULT NULL,
-`weather_code` int DEFAULT NULL,
-`wind_direction` float DEFAULT NULL,
-`wind_speed` float DEFAULT NULL,
-`valid_time` datetime(6) DEFAULT NULL,
-`weather_id` VARCHAR(36) DEFAULT NULL,
-PRIMARY KEY (`id`),
-KEY `FKntehu79l7lrpw6um6ivv4q2ep` (`weather_id`),
-CONSTRAINT `FKntehu79l7lrpw6um6ivv4q2ep` FOREIGN KEY (`weather_id`)
-REFERENCES `weather` (`id`)
-ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+create table weather_data
+(
+    id             varchar(36) not null
+        primary key,
+    precipitation  float       null,
+    temperature    float       null,
+    weather_code   int         null,
+    wind_direction float       null,
+    wind_speed     float       null,
+    valid_time     datetime(6) null,
+    weather_id     varchar(36) null,
+    constraint FKntehu79l7lrpw6um6ivv4q2ep
+        foreign key (weather_id) references weather (id)
+            on delete cascade
+);
+
 
