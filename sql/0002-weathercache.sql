910d46fb35b6f6fa62129155f37ad748933402c9
Erik <59744487+knottem@users.noreply.github.com>
2024-02-25 13:37:28 +0100

    Updated the database structure to reflect reality

--- sql/0002-weathercache.sql
+++ sql/0002-weathercache.sql
@@ -1,9 +1,5 @@
 -- noinspection SqlNoDataSourceInspectionForFile
-
--- NOT FINISHED, started this if I want the cache to be saved on the db instead of just in the program.
-
 USE `weatherdb`;
-
 -- Create the Cache table
 
 CREATE TABLE `weather` (
@@ -12,19 +8,6 @@ CREATE TABLE `weather` (
 `message` varchar(255) DEFAULT NULL,
 `city` varchar(255) DEFAULT NULL,
 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `weather_cache` (
-`id` VARCHAR(36) NOT NULL,
-`timestamp` datetime(6) DEFAULT NULL,
-`weather_id` VARCHAR(36) DEFAULT NULL,
-`cache_key` varchar(255) DEFAULT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `UK_8gfwgykmrmf3ef20ejum2iibi` (`weather_id`),
-CONSTRAINT `UK_cache_key_weather_id` UNIQUE (`cache_key`, `weather_id`),
-CONSTRAINT `FK3ssw9004cusot6y7n2jxdytcg` FOREIGN KEY (`weather_id`)
-    REFERENCES `weather` (`id`)
-    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `weather_data` (
